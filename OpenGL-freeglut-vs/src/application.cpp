569a97c93fac96aaa81d66baabed3ccc5fa9ba4b
blvu <013ayu@gmail.com>
2023-10-12 21:06:54 +0800

    Iniital Commit

---
+++ OpenGL-freeglut-vs/src/application.cpp
@@ -0,0 +1,25 @@
+#include <GL/glut.h>
+
+using namespace std;
+//testingrtr
+
+void display()
+{
+	glClear(GL_COLOR_BUFFER_BIT);
+	glBegin(GL_POLYGON);
+	glVertex2f(-0.5, -0.5);
+	glVertex2f(-0.5, 0.5);
+	glVertex2f(0.5, 0.5);
+	glVertex2f(0.5, -0.5);
+	glEnd();
+	glFlush();
+
+}
+
+int main(int argc, char** argv)
+{
+	glutInit(&argc, argv);
+	glutCreateWindow("simple");
+	glutDisplayFunc(display);
+	glutMainLoop();
+}
---
+++ README.md
@@ -0,0 +1,7 @@
+# OpenGL-freeglut-vs
+vs starter project for building opengl freeglut apps
+
+## Troubleshooting
+![image](https://github.com/6undae/OpenGL-freeglut-vs/assets/91067593/d91c20a2-67ac-479b-b864-70608bb8a6d1)
+
+- move freeglut.dll to where executable is located (usually $(SolutionDir)x64/Release|Debug/.exe)
